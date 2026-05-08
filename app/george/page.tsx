675bfb672375054ee899aff2322d44c739c20e41
Lester Sawyer <zkvsawyer@gmail.com>
2026-05-07 21:02:00 -0500

    Use transparent LIVE earbud asset

--- app/george/page.tsx
+++ app/george/page.tsx
@@ -3880,7 +3880,7 @@ return (
   {liveMode && (
     <div className="pointer-events-none flex justify-center pt-2 pb-1">
       <img
-        src="/earbud02.png"
+        src="/earbud02-transparent.png"
         alt=""
         className="h-[92px] w-auto opacity-90 drop-shadow-[0_0_30px_rgba(124,140,255,0.24)] animate-pulse"
       />
@@ -4998,7 +4998,7 @@ I will guide you in real time. Start speaking.`
   <div className="fixed inset-x-0 top-[92px] z-[260] flex justify-center pointer-events-none">
     <div className="flex flex-col items-center gap-3 px-6 text-center animate-[menuLift_700ms_ease-out]">
       <img
-        src="/earbud02.png"
+        src="/earbud02-transparent.png"
         alt=""
         className="h-[118px] w-auto opacity-90 drop-shadow-[0_0_34px_rgba(124,140,255,0.26)] animate-pulse"
       />
