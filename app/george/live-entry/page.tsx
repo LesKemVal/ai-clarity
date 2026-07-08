3e5e92590d3e6eca0d2ccf585880c48761644739
Lester Sawyer <zkvsawyer@gmail.com>
2026-07-08 11:58:10 -0500

    Restore LIVE Entry page flow

--- app/george/live-entry/page.tsx
+++ app/george/live-entry/page.tsx
@@ -1,11 +1,5 @@
 import LiveEntryClient from './LiveEntryClient'
-import LiveEntryReceiverProfileEnhancer from '@/components/george/live/LiveEntryReceiverProfileEnhancer'
 
 export default function LiveEntryPage() {
-  return (
-    <>
-      <LiveEntryReceiverProfileEnhancer />
-      <LiveEntryClient />
-    </>
-  )
+  return <LiveEntryClient />
 }
